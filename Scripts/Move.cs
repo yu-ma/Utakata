3bdf17ea3c4650ced1db02dd9ce962a3a2663605
Yusuke-Mano <martheid1889@gmail.com>
2018-08-15 22:31:54 +0900

    First Commit

---
+++ Scripts/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed = 5.0f;//移動速度
+    private Vector3 velocity;   //移動方向
+    
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            velocity.z -= 1;
+        if (Input.GetKey(KeyCode.A))
+            velocity.x += 1;
+        if (Input.GetKey(KeyCode.S))
+            velocity.z += 1;
+        if (Input.GetKey(KeyCode.D))
+            velocity.x -= 1;
+
+        velocity = velocity.normalized * moveSpeed * Time.deltaTime;
+
+        //いずれかの方向に移動している場合
+        if (velocity.magnitude > 0)
+        {
+            //プレイヤー位置の更新
+            transform.position += velocity;
+        }
+    }
+}
